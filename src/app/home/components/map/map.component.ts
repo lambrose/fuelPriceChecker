fb6e6184cae0ffd8ea7e52882226693216b84e92
Louis Ambrose <lambrosetya@gmail.com>
2022-07-28 22:15:53 +0100

    refactor and add styling

--- src/app/home/components/map/map.component.ts
+++ src/app/home/components/map/map.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-// import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { Observable } from 'rxjs';
 
